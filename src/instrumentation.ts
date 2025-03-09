1c4ea71ff638c929a4755013fdc9293db6211917
Prateek-Wayne <lordprateekverma@gmail.com>
2025-03-09 18:57:22 +0530

    Add OpenTelemetry collector configuration and update instrumentation for metrics, logs, and traces

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -15,18 +15,28 @@ import {
   ConsoleLogRecordExporter,
   SimpleLogRecordProcessor,
 } from '@opentelemetry/sdk-logs';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto';
+import { OTLPMetricExporter } from '@opentelemetry/exporter-metrics-otlp-proto';
+import { OTLPLogExporter } from '@opentelemetry/exporter-logs-otlp-proto';
 
 const sdk = new NodeSDK({
   resource: new Resource({
     [ATTR_SERVICE_NAME]: 'dice-roll app',
     [ATTR_SERVICE_VERSION]: '1.0.0',
   }),
-  traceExporter: new ConsoleSpanExporter(),
+  //   traceExporter: new ConsoleSpanExporter(),//to print in the console
+  traceExporter: new OTLPTraceExporter(),
+  // {url}
+  // url:we can give url like this
   metricReader: new PeriodicExportingMetricReader({
-    exporter: new ConsoleMetricExporter(),
+    // exporter: new ConsoleMetricExporter(),// to print in  the console
+    exporter: new OTLPMetricExporter(),
   }),
   logRecordProcessors: [
-    new SimpleLogRecordProcessor(new ConsoleLogRecordExporter()),
+    new SimpleLogRecordProcessor(
+      // new ConsoleLogRecordExporter() // to print in the console
+      new OTLPLogExporter()
+    ),
   ],
   instrumentations: [getNodeAutoInstrumentations()],
 });
